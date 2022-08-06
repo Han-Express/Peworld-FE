7f8ec0f9333a2c9a4290435fb7f160705f459696
han <nahrowi.one@gmail.com>
2022-08-06 21:44:11 +0700

    feat: landing page

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,11 +1,11 @@
 import Layout from '../Component/Layout'
 import Link from 'next/link'
+import LandingPageLayout from '../Layout/landing-page'
 
 export default function Home() {
   return (
     <Layout>
-      <div className="underline text-blue-800"><Link href="/profile">Ini Home ngelink ke profile</Link></div>
-      <div className="underline text-blue-800"><Link href="/profile/comp-profile">Ini Home ngelink ke profile company</Link></div>
+     <LandingPageLayout />
     </Layout>
   )
 }
