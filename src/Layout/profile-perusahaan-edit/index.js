1605a8124b5a9485136936c25186e4889d31e4c4
han <nahrowi.one@gmail.com>
2022-08-07 21:01:59 +0700

    feat: responsive perusahaan edit

--- src/Layout/profile-perusahaan-edit/index.js
+++ src/Layout/profile-perusahaan-edit/index.js
@@ -7,26 +7,26 @@ const ProfilePerusahaanLayout = () => {
         <>
            
             <main className="flex flex-col">
-                <div className="bg-[#5E50A1] w-full h-96 relative">
+                <div className="bg-[#5E50A1] md:w-auto h-96 relative">
                 </div>
-                <div className="flex mx-32 absolute mt-16">
-                    <div className="w-2/5 h-full  mr-10 rounded-lg bg-white ">
-                        <div className="flex flex-col px-1 py-2 rounded-2xl mb-5 shadow-2xl">
-                            <div className="flex flex-row justify-center mt-10">
+                <div className="lg:flex lg:mx-32 px-3 absolute mt-16">
+                    <div className="md:w-1/3 h-full  md:mr-8 rounded-lg bg-white ">
+                        <div className="md:flex flex-col px-1 py-2 rounded-2xl mb-5 shadow-2xl">
+                            <div className="flex flex-row justify-center mt-5">
                                 <div class="avatar">
-                                    <div class="w-52 rounded-full">
+                                    <div class="w-36 rounded-full">
                                         <img src="https://placeimg.com/192/192/people" />
                                     </div>
                                 </div>
                             </div>
                             <div className="flex flex-row justify-center my-5">
-                                <img className="w-5 mr-3" src="/img/edit.svg" />
-                                <p className="text-2xl font-bold text-[#9EA0A5]">
+                                <img className="w-4 mr-3" src="/img/edit.svg" />
+                                <p className="text-xl font-bold text-[#9EA0A5]">
                                     EDIT
                                 </p>
                             </div>
-                            <div className="flex flex-col mx-16">
-                                <h1 className="text-2xl font-bold mb-3 mt-10">
+                            <div className="md:flex flex-col mx-10">
+                                <h1 className="text-xl font-bold mb-5 mt-7">
                                     PT. Martabat Jaya Abadi
                                 </h1>
                                 <p className="mb-3">
@@ -44,10 +44,10 @@ const ProfilePerusahaanLayout = () => {
                             </div>
                         </div>
                         <div className="">
-                            <button className="bg-[#5E50A1] p-4 w-full text-xl font-bold rounded-md mb-3 mt-5 text-white">
+                            <button className="bg-[#5E50A1] px-4 py-4 w-full text-xl font-bold rounded-md mb-3 mt-5 text-white">
                                 Simpan
                             </button>
-                            <button className="border-[#5E50A1] border-2 p-4 w-full text-xl font-bold rounded-md mb-3 mt-5 text-[#5E50A1]">
+                            <button className="border-[#5E50A1] border-2 px-4 py-4 w-full md:text-xl font-bold rounded-md mb-3 mt-5 text-[#5E50A1]">
                                 Kembali
                             </button>
                         </div>
@@ -57,7 +57,7 @@ const ProfilePerusahaanLayout = () => {
                     {/* </div> */}
 
 
-                    <div className="w-3/5 h-full flex flex-col">
+                    <div className="md:w-2/3 h-full flex flex-col mb-10">
                         <div className="bg-white rounded-lg shadow-2xl">
                             <div className="border-b-2">
                                 <h1 className="font-bold text-2xl py-4 ml-10">
@@ -203,7 +203,7 @@ const ProfilePerusahaanLayout = () => {
                                     </div>
                                 </div>
                                 <div className="mt-5">
-                                    <div className="flex flex-col">
+                                    <div className="flex flex-col mb-10">
                                         <p className="mb-3 font-bold text-base">
                                             Linkedin :
                                         </p>
