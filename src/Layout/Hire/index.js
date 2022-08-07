db9e72555fca1fcd40e479a876a6ae2424aa6032
Dio Fitriadi <fitriadidio@gmail.com>
2022-08-07 20:02:43 +0700

    add responsive

--- src/Layout/Hire/index.js
+++ src/Layout/Hire/index.js
@@ -6,10 +6,11 @@ const HirePage = () => {
   return(
     <>
     <Layout>
+    <div>
     <div className="bg-slate-100 pb-16">
-      <div className="bg-violet-700 h-80 w-full"></div>
-        <div className="my-5 flex w-4/6 mx-auto -mt-64">
-          <div className="bg-white rounded-xl p-6 w-7/12 border-2 mr-10 justify-around">
+      <div className="bg-violet-700 h-80 md:w-full mx-auto rounded-xl w-11/12"></div>
+        <div className="my-5 flex-row md:flex md:w-4/6 w-11/12 mx-auto -mt-64">
+          <div className="bg-white md:w-4/6 p-6 w-full mr-10 justify-around md:rounded-xl">
             <div>
               <img className="mx-auto" src="/img/companyProfile.png"/>
             </div>
@@ -26,7 +27,7 @@ const HirePage = () => {
               <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vestibulum erat orci, mollis nec gravida sed, ornare quis urna. Curabitur eu lacus fringilla, vestibulum risus at.
               </p>
             </div>
-            <button className="bg-violet-700 hover:opacity-90 transition p-2 my-6 rounded-md w-11/12 text-white font-semibold">Hire</button>
+            <button className="bg-violet-700 hover:opacity-90 transition p-2 my-6 rounded-md w-full text-white font-semibold">Hire</button>
             <div>
               <div className="text-lg font-semibold">
                 <p>Skill</p>
@@ -44,7 +45,7 @@ const HirePage = () => {
               </div>
             </div>
           </div>
-        <div className="flex flex-col bg-white rounded-xl p-5 w-full h-[80vh]">
+        <div className="flex flex-col bg-white rounded-xl p-5 w-full h-[80vh] md:mt-0 mt-10">
           <div className="p-1">
             <h1 className="text-2xl font-semibold mb-4">Hubungi Lous Tomlinson</h1>
             <p className="text-sm font-normal mb-8 text-slate-500">Lorem ipsum dolor sit amet, consectetur adipiscing elit. In euismod ipsum et dui rhoncus auctor.</p>
@@ -60,12 +61,13 @@ const HirePage = () => {
                 <label className="text-slate-600" for="comment">Pesan :</label>
                 <textarea className="w-full border-[1px] rounded-md h-4/6 border-violet-700 p-2 mt-3" name="comment" placeholder="tinggalkan pesan disini"></textarea>
                 <br/>
-                <input className=" bg-violet-700 px-2 py-1 w-full mx-auto mt-4 rounded-lg hover:cursor-pointer text-white" type="submit" value="Kirim" />
+                <input className=" bg-violet-700 p-2 w-full mx-auto mt-4 rounded-lg hover:cursor-pointer text-white" type="submit" value="Kirim" />
               </form>
             </div>
           </div>
         </div>
       </div>
+    </div>
     </div>
     </Layout>
     </>
