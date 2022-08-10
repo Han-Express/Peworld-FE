269677ad675e75a73071d837910479c6693d5b85
han <nahrowi.one@gmail.com>
2022-08-11 04:12:03 +0700

    feat : update home

--- src/redux/action/talent.js
+++ src/redux/action/talent.js
@@ -29,7 +29,7 @@ export const GetTalent = (params, skill) => {
     dispatch(GetTalentRequest())
         axios({
             method: "GET",
-            url: `http://coral-app-3yjfb.ondigitalocean.app/api/v1/employees?skill=${skill}${page ? `&page=${page}`:``}${limit? `&limit=${limit}`:``}${job_status? `&job_status=${job_status}`:``}`
+            url: `https://coral-app-3yjfb.ondigitalocean.app/api/v1/employees?skill=${skill}${page ? `&page=${page}`:``}${limit? `&limit=${limit}`:``}${job_status? `&job_status=${job_status}`:``}`
             // `http://localhost:3003/api/v1/movie/?${page ? `page=${page}`:``}${limit? `&limit=${limit}`:``}${title? `&title=${title}`:``}`,
         })
         .then ((res) => {
